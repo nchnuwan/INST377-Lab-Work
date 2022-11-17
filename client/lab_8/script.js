1634391fd55c902792d9502f3556cb1f0764ed5e
nchnuwan <nhewabet@terpmail.umd.edu>
2022-11-17 18:05:07 -0500

    final add

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -72,7 +72,7 @@ function filterList(array, filterInputValue) {
 
 function initMap() {
   console.log('initMap');
-  const map = L.map('map').setView([38.9897, -76.9378], 10);
+  const map = L.map('map').setView([38.9897, -76.9378], 13);
 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
